94901f0c085ec3da1ea2930bbdefa73ca89f4191
Anderson Santos <92414470+afsantos2@users.noreply.github.com>
2023-06-24 15:45:53 -0300

    renderiza somente um item (post)

--- src/js/index.js
+++ src/js/index.js
@@ -3,24 +3,40 @@ import dados from '../dados-postagem.json' assert { type: 'json'}
 addEventListener('DOMContentLoaded', postGerador)
 
 function postGerador() {
-  const postagens = dados.posts 
-  const $postLimite = document.querySelector('.js-post-limite')
+  const postagens = dados.posts
+  const $post = document.querySelector('.js-post-limite')
   const main = document.querySelector('main')
 
-  if ($postLimite) clonar($postLimite)
+  if ($post) clonar($post)
 
-  function clonar($alvo) { 
-    postagens.forEach(post => {
+  function clonar($alvo) {
+    postagens.forEach((post, i) => {
       const $clone = $alvo.cloneNode(true)
-      $clone.id = post.id
+      $clone.dataset.cloneId = post.id
 
+      // dados post header
       $clone.querySelector('.js-usuario-img').src = post.url_thumb
       $clone.querySelector('.js-post-username').innerText = post.nome
       $clone.querySelector('.js-post-nickname').innerText = post.nick
       $clone.querySelector('.js-post-img').src = post.url_img         
       $clone.querySelector('.js-post-tempo time').innerText = post.tempo
+
+      // statisticas do post
+      const statClasses = [
+        '.js-post-stat-coments',
+        '.js-post-stat-retweets',
+        '.js-post-stat-likes',
+        '.js-post-stat-views'
+      ]
       
-      main.appendChild($clone)
+      const teste = statClasses.map(classe => {
+        $clone.querySelector(classe).innerText = Math.floor(Math.random() * 1000)        
+      })
+      
+      // renderiza somente um item
+      if (i < 1) {
+        main.appendChild($clone)
+      }      
     })
   }
 }
